eb5bdbd67181b3dec9311fccd48294db01502ed9
auccapuclla <auccapuclla2013@gmail.com>
2023-06-06 20:07:52 +0800

    HW2-Q1-2-3

--- cohorts/2023/02-experiment-tracking/homework/hpo.py
+++ cohorts/2023/02-experiment-tracking/homework/hpo.py
@@ -8,7 +8,7 @@ from optuna.samplers import TPESampler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("random-forest-hyperopt")
 
 
@@ -46,8 +46,9 @@ def run_optimization(data_path: str, num_trials: int):
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
+        # print(rmse)
         rmse = mean_squared_error(y_val, y_pred, squared=False)
-
+        mlflow.log_metric("rmse", rmse)
         return rmse
 
     sampler = TPESampler(seed=42)
